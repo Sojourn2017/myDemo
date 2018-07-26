804f06d93689bb08023b575e9e6675afde667471
Bilin008 <liezh@mybilin.com>
2018-07-26 18:18:59 +0800

    action-view

---
+++ echarts-test/src/js/action-view.js
@@ -0,0 +1,17 @@
+var app =  new Vue({
+  el: '#app',
+  data: {
+    chart: 'chart',
+    chartTitle: ['销售量', '销售额', 'TOTAL']
+  },
+  methods: {
+
+  }
+})
+
+// // echarts初始化
+// var actionChart = echarts.init(document.getElementById('action-chart'));
+
+// var opt = {
+
+// }
