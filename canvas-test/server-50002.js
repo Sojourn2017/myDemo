e1b208536faafa70791687ed30e2ab6d168e2e29
Bilin008 <liezh@mybilin.com>
2018-11-21 18:13:44 +0800

    add a few canvas demo

---
+++ canvas-test/server-50002.js
@@ -0,0 +1,41 @@
+/*
+ * 提供静态页面访问服务
+ */
+
+let http = require("http");
+let url = require("url");
+let fs = require("fs");
+
+let server = http.createServer((req, res) => {
+  let pathName = url.parse(req.url).pathname;
+
+  let rePathName = `./${pathName}`;
+
+  fs.readFile(rePathName, (err, data) => {
+    if (err) {
+      fs.readFile("./index.html", (err1, data1) => {
+        if (err1) {
+          res.writeHead(404, {
+            "Context-Type": `text/html`
+          });
+          res.end("404 Not Found");
+        } else {
+          res.writeHead(200, {
+            "Context-Tpye": `text/html`
+          });
+          res.end(data1);
+        }
+      });
+    } else {
+      res.writeHead(200, {
+        "Context-Tpye": `text/html`
+      });
+
+      res.end(data);
+    }
+  });
+});
+
+server.listen(50002, "0.0.0.0", () => {
+  console.log("server running at port 50002");
+});
