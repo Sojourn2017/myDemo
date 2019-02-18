027a608899eb9e37c40c40f612e7518ea2991751
Sojourn <sojourn2017@163.com>
2019-02-18 10:31:43 +0800

    update translate-server-test: change logs path

--- translate-server-test/src/util/writeLog.js
+++ translate-server-test/src/util/writeLog.js
@@ -1,15 +1,34 @@
 /**
  * 访问日志
- */ 
+ */
+
 import fs from "fs";
 
+function createDir(dirArr) {
+  let createPathList = dirArr.filter(p => {
+    return !fs.existsSync(p);
+  });
+  if (createPathList.length === 0) return;
+
+  createPathList.forEach(p => {
+    try {
+      fs.mkdirSync(p);
+    } catch (e) {
+      console.log(e);
+    }
+  });
+}
+
 function writeErr(e) {
-  fs.appendFile("./src/log/error.txt", `${new Date()}: ${e}\r\n`, e => {});
+  let handleType = fs.existsSync('./logs/error.log') ? 'appendFile' : 'writeFile';
+  fs[handleType]("./logs/error.log", `${new Date()}: ${e}\r\n`, e => {});
 }
 
 export default function(log, isWriteErr) {
-  let p = isWriteErr ? "./src/log/error.txt" : "./src/log/log.txt";
-  fs.appendFile(p, `${new Date()}: ${log} \r\n`, err => {
+  createDir(["./logs"]);
+  let p = isWriteErr ? "./logs/error.log" : "./logs/log.log";
+  let handleType = fs.existsSync(p) ? 'appendFile' : 'writeFile';
+  fs[handleType](p, `${new Date()}: ${log} \r\n`, err => {
     if (err) {
       writeErr(err);
     }
