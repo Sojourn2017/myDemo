be305b4cdd74c848a162458e8292903bda70a8ea
Sojourn2017 <1195110630@qq.com>
2019-07-10 00:42:27 +0800

    add docs

---
+++ docs/vscode-setting-sync-test.js
@@ -0,0 +1,34 @@
+/**
+ * GitHub Token: 834f3f0430d3dacdfd4decc8ccd30aa56f86807d
+ * GitHub Gist: 5a412f541aa8e974dd668e86f615c608
+ */
+
+/**
+ * Extensions Added:
+ * change-case v1.0.0
+ * cocos-creator v0.0.1
+ * cocos-debug v0.0.1
+ * code-settings-sync v3.2.9
+ * githistory v0.4.6
+ * gitlens v9.7.3
+ * html5-boilerplate v1.0.3
+ * icon-fonts v2.1.5
+ * markdown-preview-enhanced v0.3.13
+ * minify v0.4.3
+ * open-in-browser v2.0.0
+ * prettier-vscode v1.9.0
+ * python v2019.3.6558
+ * quokka-vscode v1.0.212
+ * regex v0.2.0
+ * vetur v0.20.0
+ * vscode-babel-coloring v0.0.4
+ * vscode-css-peek v2.2.0
+ * vscode-eslint v1.8.2
+ * vscode-faker v1.3.0
+ * vscode-icons v8.6.0
+ * vscode-language-pack-zh-hans v1.32.4
+ * vscode-react-native v0.9.2
+ * vscode-react-native v0.10.0
+ * vscode-svgviewer v2.0.0
+ * vscode-todo-highlight v1.0.4
+ */
